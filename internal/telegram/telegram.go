ad594207cdf5e319b2545a9631bc635b24ebe0ce
Varnava <alekseyyvarnava@yandex.ru>
2024-12-11 15:21:40 +0500

    Добавлена возможность писать боту в общие чаты

--- internal/telegram/telegram.go
+++ internal/telegram/telegram.go
@@ -38,3 +38,24 @@ func TgAPI_SendMessage(chatID int64, message string) (bool, error) {
 	}
 	return true, nil
 }
+
+func StartBot() {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // Если получили сообщение
+			chatID := update.Message.Chat.ID
+			chatType := update.Message.Chat.Type
+			log.Printf("Получено сообщение из чата %d (тип: %s): %s", chatID, chatType, update.Message.Text)
+
+			if chatType == "group" || chatType == "supergroup" {
+				fmt.Println(chatID)
+				msg := "Бот TimeTask добавлен в общий чат! Теперь он может отправлять сюда сообщения."
+				TgAPI_SendMessage(chatID, msg)
+			}
+		}
+	}
+}
